bdbc704d8cfc806f9e9568f106f7fe162fb2e2db
Hisamatsu Haruki <89366502+H-haruki-20@users.noreply.github.com>
2023-03-06 11:31:42 +0900

    hide my api key

---
+++ analyze.py
@@ -0,0 +1,29 @@
+from janome.tokenizer import Tokenizer
+import openai
+
+openai.api_key = "Your OpenAI's api key"
+
+def analyze_text(text):
+    t = Tokenizer()
+    tokens = t.tokenize(text)
+    result = []
+    # MeCabで形態素解析を行う
+    for token in tokens:
+        print(token)
+        result.append(token)
+    
+    return result
+    
+def openAI():
+    # プロンプト
+    prompt = '''
+    '''
+
+    # 推論
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": '次の日本語を言い換えてください: “ガリガリ"'}
+    ]
+    )
+    print(response["choices"][0]["message"]["content"])
