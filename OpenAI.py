8c643dd87334657e251f2c05760d29deb9fcf5ca
Hisamatsu Haruki <89366502+H-haruki-20@users.noreply.github.com>
2023-03-26 13:36:50 +0900

    Change Files

--- OpenAI.py
+++ OpenAI.py
@@ -11,5 +11,71 @@ if "OPENAI_API_KEY" not in os.environ:
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 def ToFuwafuwa(word:str):
-    model = gensim.models.KeyedVectors.load_word2vec_format('model.vec', binary=False)
-    lst = model.most_similar(positive=[word],negative=[""])
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {"role":"user","content":f"「{word}」を攻撃性の低い文章に変換してください"}
+        ]
+    )
+
+    answer = response["choices"][0]["message"]["content"]
+    print(f"攻撃性の低い文章 : {answer}")
+
+    # response = openai.ChatCompletion.create(
+    #     model = "gpt-3.5-turbo",
+    #     messages = [
+    #     {"role":"user","content":"以下の文章をポジティブに変換してください"},
+    #     {"role":"assistant","content":word}
+    #     ]
+    # )
+
+    # answer = response["choices"][0]["message"]["content"]
+
+    # print(f"ポジティブに変換 : {answer}" )
+
+    return answer
+
+def ToMoreFuwafuwa(text:str):
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+        {"role":"user","content":"以下の文章をポジティブに変換してください"},
+        {"role":"assistant","content":text}
+        ]
+    )
+
+    answer = response["choices"][0]["message"]["content"]
+
+    print(f"ポジティブに変換 : {answer}" ) 
+    return answer
+
+
+def ToFuwafuwa2(text):
+    MESSAGE = ""
+    with open("black_word_list.txt","r") as f:
+        black_word = f.readlines()
+        for i in range(len(black_word)):
+            target = black_word[i].replace("\n","")
+            MESSAGE += f"{target},"
+
+    MESSAGE += """
+    はネガティブな言葉です．それを踏まえて以下の文章を攻撃性の低い言い方に変換してください．
+    """
+
+    print(MESSAGE)
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+        {"role":"user","content":MESSAGE},
+        {"role":"assistant","content":text}
+        ],
+        top_p = 0.6
+    )
+
+    answer = response["choices"][0]["message"]["content"]
+
+    print(f"第一変換 : {answer}" )
+
+    
+
+    return answer 
--- app.py
+++ app.py
@@ -1,32 +1,107 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from analyze import *
 from OpenAI import *
+import random
+import csv
+
+import re
+
+COUNT_MAX = 10
 
 app = Flask(__name__)
 
-@app.route("/test")
-def hello_world():
-    return render_template("index.html")
+@app.route("/more-fuwafuwa", methods = ["GET","POST"])
+def fuwafuwa():
+    if request.method == "GET":
+        with open("text.txt","r") as f:
+            r = f.readline()
+            text = r.replace("\n","")
+            print(f"もっとふわふわにするテキストはこちら : {text}")
+        answer_from_GPT = ToMoreFuwafuwa(text)
+        return render_template("result.html",text=answer_from_GPT)
+
+@app.route("/report", methods = ["GET","POST"])
+def report():
+    if request.method == "POST":
+        text = request.form.get("report_text")
+        print(f"ユーザーから「{text}」がちくちく言葉として報告されました")
+
+        with open("data/report.csv","at") as f:
+            writer = csv.writer(f)
+            writer.writerow([text,0])
+
+        #report 押したときの処理 (thanks.htmlを表示)
+        return render_template("thanks.html")
+
+    # report押したときの処理 (thanks.htmlを表示する?)
+    with open("text.txt","r") as f:
+        r = f.readline()
+        text = r.replace("\n","")
+        print(f"text : {text}")
+    answer_from_GPT = ToFuwafuwa(text)
+    return render_template("result.html",text=answer_from_GPT)
 
 
+@app.route("/count", methods = ["GET","POST"])
+def count():
+    if request.method == "GET":
+        req = request.args
+        num = int(req.get("num"))
+        with open('data/report.csv', 'r') as file:
+            reader = csv.reader(file)
+            data = list(reader)
+        print(data)
+        value = int(data[num][1]) + 1
+        data[num][1] = value
+        if value == COUNT_MAX:
+            # データセット << どんな内容をCSVファイルに書くか >>
+            with open("data/fine_tuning.csv","a") as f:
+                writer = csv.writer(f)
+                print(f"10回以上{[data[num][0]]}は「はい」が押されたよ")
+                writer.writerow([data[num][0]])
+
+        # 書き換えたデータをCSVファイルに保存する
+        with open('data/report.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
+        return render_template("index-no.html")
+    else:
+        # 入力されたテキストを取得する
+        text = request.form.get("input_text")
+        with open("text.txt","w") as f:
+            f.write(text)
+        answer_from_GPT = ToFuwafuwa(text)
+        if "「" in answer_from_GPT:
+            answer_from_GPT = re.findall("(?<=「).+?(?=」)", answer_from_GPT)[0]
+
+        # 解析結果を表示する
+        return render_template("result.html",text=answer_from_GPT) 
+    
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         # 入力されたテキストを取得する
         text = request.form.get("input_text")
-        
-        result,word = ProfanityFilter(text)
-        print(f"result : {result}")
-        print(f"word : {word}")
-        if result:
-            # 悪口の部分の言い換えを行う (Chat-GPT)
-            final = text.replace(word,"ペンギン")
-        
+        with open("text.txt","w") as f:
+            f.write(text)
+        answer_from_GPT = ToFuwafuwa(text)
+        if "「" in answer_from_GPT:
+            answer_from_GPT = re.findall("(?<=「).+?(?=」)", answer_from_GPT)[0]
+
         # 解析結果を表示する
-        return render_template("result.html",text=final)
+        return render_template("result.html",text=answer_from_GPT)
     
     else:
-        return render_template("index.html")
+        targets = []
+        with open("data/report.csv","r") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                targets.append(row[0])
+            num = random.randint(0,len(targets)-1)
+            target = targets[num].replace("\n","")
+            print(f"target : {target}")
+            
+        return render_template("index.html",target=targets[num],num=num)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
